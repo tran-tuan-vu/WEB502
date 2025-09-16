9706d76210fe57379de3922101fbb8970b9ae687
TuanVu <chipucutvu123@gmail.com>
2025-09-16 20:16:22 +0700

    init

---
+++ script.js
@@ -0,0 +1,10 @@
+// let myName: string = 'vutt';
+// let myAge: number;
+// myAge = 21;
+// console.log(myName, myAge);
+function chuViHCN(dai, rong) {
+    return (dai + rong) * 2;
+}
+var dai = 20;
+var rong = 10;
+console.log("Chu vi hinh chu nhat la: " + chuViHCN(dai, rong));
---
+++ script.ts
@@ -0,0 +1,11 @@
+// let myName: string = 'vutt';
+// let myAge: number;
+// myAge = 21;
+// console.log(myName, myAge);
+function chuViHCN(dai:number, rong:number) {
+    return (dai + rong) * 2;
+}
+
+let dai = 20;
+let rong = 10;
+console.log("Chu vi hinh chu nhat la: " + chuViHCN(dai, rong));
